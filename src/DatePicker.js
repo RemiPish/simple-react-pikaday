a1c60228125c056b2c6d5ea52b852c7dd4c0e4b4
UNKNOWN <>
2024-02-24 04:23:04 +0100

    first commit
    change
    
    name change
    
    same error
    
    Name error fix
    
    fix
    
    lul

---
+++ src/DatePicker.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react';
+import Pikaday from 'pikaday';
+import 'pikaday/css/pikaday.css';
+import moment from 'moment';
+
+const DatePicker = ({ value, onChange }) => {
+  const ref = useRef(null);
+  const picker = useRef(null);
+
+  useEffect(() => {
+    picker.current = new Pikaday({
+      field: ref.current,
+      format: 'YYYY-MM-DD',
+      onSelect: function() {
+        const date = this.getMoment().format('YYYY-MM-DD');
+        onChange(date);
+      }
+    });
+
+    return () => {
+      picker.current.destroy();
+    };
+  }, [onChange]);
+
+  useEffect(() => {
+    if (value && picker.current) {
+     
+      const date = moment(value, 'YYYY-MM-DD').toDate();
+      picker.current.setDate(date, true); 
+    }
+  }, [value]);
+
+  return <input type="text" ref={ref} />;
+};
+
+export default DatePicker;
